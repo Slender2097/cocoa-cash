54955ab88262d6dd0d50cee78fad643926d40bf2
unknown <jose2097pm@gmail.com>
2026-04-15 18:45:56 -0600

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 // src/pages/_app.jsx
-import "@/styles/globals.css";
+/*import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+}*/
+
+import "@/styles/globals.css";      
+import { Analytics } from "@vercel/analytics/react";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />           
+    </>
+  );
 }
